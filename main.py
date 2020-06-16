4d0fd74a59ad319dfcf1edaaa2eda9c4379a69df
m8torres <m8torres@enib.fr>
2020-06-16 12:05:21 +0200

    resize windows

--- main.py
+++ main.py
@@ -23,15 +23,17 @@ from signal_visualizer import *
 from wave_generator import *
 
 mw = tk.Tk()
-mw.geometry("600x300")
-#mw.attributes("-fullscreen", True)
-mw.title("Leçon de Piano")
+height = mw.winfo_screenheight()
+width = mw.winfo_screenwidth()
+geometry = str(width) + "x" + str(height)
+mw.geometry(geometry)
+mw.title("Piano Lesson")
 frame=tk.Frame(mw,borderwidth=5,width=360,height=300)
 octaves=4
 piano=Piano(frame,octaves)
 
 view=View(frame)
-view.grid(4)
+view.grid(8)
 
 waveGenerator=WaveGenerator(frame,view,piano)
 waveGenerator.packing()
@@ -63,9 +65,20 @@ menubar.add_cascade(label="File", menu=filemenu)
 
 # help dropdown menu
 helpmenu = tk.Menu(menubar, tearoff=0)
+def lib_application():
+    messagebox.showinfo("About","This application has:\n  a piano\n  a signal visualiser\n  and a wave generator"
+                                "and each of them can be played individualy.\n\n"
+                                "In this main application, you'll be able to generate the notes\n"
+                                "you wish and play them either from the piano or the 'Play Note' button.\n"
+                                "For the chords, you don't need to know how to form them, just chose the\n"
+                                "note and whether the chord is Minor or Mayor! You can play them from the\n"
+                                "'Play Chord' button or from the piano too.\n"
+                                "You will also be able to see the signal the note you want to generate\n"
+                                "produces at the lower part of the window.\n")
+helpmenu.add_command(label="About", command=lib_application)
 def about_application():
     messagebox.showinfo("Authors","Perez, Jeronimo\nMia Torres Lopez\nENIB - CAI\nProfessor: Alexis Nédélec")
-helpmenu.add_command(label="About", command=about_application)
+helpmenu.add_command(label="Creators", command=about_application)
 def lib_application():
     messagebox.showinfo("Libraries","TkInter\n   - wiki.python.org/moin/TkInter\n"
                                     "Winsound (sound in Windows)\n   - docs.python.org/2/library/winsound.html\n"
